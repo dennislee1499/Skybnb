45b43a2610e839a44ce58b4040a2f2aaa055af67
Dennis Lee <dennislee1499@gmail.com>
2024-04-02 00:54:15 -0400

    Add owner to place schema

--- backend/models/Place.js
+++ backend/models/Place.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const placeSchema = new mongoose.Schema({
+const PlaceSchema = new mongoose.Schema({
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     title: String,
     address: String, 
     photos: [String],
@@ -12,6 +13,6 @@ const placeSchema = new mongoose.Schema({
     maxGuests: Number, 
 });
 
-const PlaceModel = mongoose.model('Place', placeSchema);
+const PlaceModel = mongoose.model('Place', PlaceSchema);
 
 module.exports = PlaceModel;
