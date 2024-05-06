27ef9f056fdd02f999ef097bc08a1ce366874079
Dennis Lee <dennislee1499@gmail.com>
2024-05-06 00:14:35 -0400

    fix positioning of show all photos button

--- frontend/src/AccomodationGallery.jsx
+++ frontend/src/AccomodationGallery.jsx
@@ -46,7 +46,7 @@ export default function AccomodationGallery({ accomodation }) {
                         </div>
                     </div>
                 </div>
-                <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-6 right-2 py-1 px-4 bg-white rounded-2xl border border-black">
+                <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-10 right-11 py-1 px-4 bg-white rounded-2xl border border-black">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clip-rule="evenodd" />
                     </svg>
