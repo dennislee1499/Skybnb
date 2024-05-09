972f690d4cca304b243e23a2460aa32d456b9bfb
Dennis Lee <dennislee1499@gmail.com>
2024-05-08 23:22:18 -0400

    Fix styling of images on accomodation gallery page

--- frontend/src/AccomodationGallery.jsx
+++ frontend/src/AccomodationGallery.jsx
@@ -26,32 +26,38 @@ export default function AccomodationGallery({ accomodation }) {
     }
 
     return (
-        <div className="relative">
-                <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden p-8">
-                    <div>
-                        {accomodation.photos?.[0] && (
-                            <div>
-                                <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + accomodation.photos?.[0]} alt="" />
-                            </div>
-                        )}
-                    </div>
-                    <div className="grid">
-                        {accomodation.photos?.[1] && (
-                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + accomodation.photos?.[1]} alt="" />
-                        )}
-                        <div className="overflow-hidden">
-                            {accomodation.photos?.[2] && (
-                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={'http://localhost:4000/uploads/' + accomodation.photos?.[2]} alt="" />
-                        )}
-                        </div>
-                    </div>
+        <div className="relative max-w-6xl mx-auto">
+            <div className="grid grid-cols-2 gap-2 items-stretch">
+
+                {accomodation.photos?.[0] && (
+                    <img
+                        onClick={() => setShowAllPhotos(true)}
+                        src={`http://localhost:4000/uploads/${accomodation.photos[0]}`}
+                        alt=""
+                        className="w-full h-95 object-cover rounded-lg cursor-pointer"
+                    />
+                )}
+
+                <div className="grid grid-cols-2 grid-rows-2 gap-2 w-full">
+                    {accomodation.photos?.slice(1, 5).map((photo, index) => (
+                        <img
+                            key={index}
+                            onClick={() => setShowAllPhotos(true)}
+                            src={`http://localhost:4000/uploads/${photo}`}
+                            alt=""
+                            className="w-full h-47 object-cover rounded-lg cursor-pointer"
+                        />
+                    ))}
                 </div>
-                <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-10 right-11 py-1 px-4 bg-white rounded-2xl border border-black">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                        <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clip-rule="evenodd" />
-                    </svg>
-                    Show all photos
-                </button>
+                <button 
+                onClick={() => setShowAllPhotos(true)} 
+                className="flex items-center gap-1 absolute bottom-5 right-12 py-1 px-4 bg-white rounded-2xl border border-black text-black shadow-md">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clip-rule="evenodd" />
+                </svg>
+                Show all photos
+            </button>
             </div>
-    )
+        </div>
+    );
 }
--- frontend/src/pages/AccomodationPage.jsx
+++ frontend/src/pages/AccomodationPage.jsx
@@ -22,22 +22,24 @@ export default function AccomodationPage() {
 
 
     return (
-        <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
-            <h1 className="text-2xl font-bold">{accomodation.title}</h1>
-            <AccomodationGallery accomodation={accomodation} />
-            <h2 className="font-semibold mt-2">{accomodation.address}</h2>
-            <div className="mt-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
-                    <div>
-                        <div className="my-5">
-                            <h2 className="font-semibold text-xl">Description</h2>
-                            {accomodation.description}
-                        </div>
-                        Check-In: {accomodation.checkIn} <br />
-                        Check-Out: {accomodation.checkOut} <br />
-                        Max Guests: {accomodation.maxGuests} <br />
-                    </div>
-                    <div>
-                        <Booking accomodation={accomodation}/>
+        <div className="mt-6 bg-gray-100 pt-4">
+            <div className="max-w-6xl mx-auto">
+                <h1 className="text-2xl font-bold my-2">{accomodation.title}</h1>
+                    <AccomodationGallery accomodation={accomodation} />
+                    <h2 className="font-semibold mt-2">{accomodation.address}</h2>
+                    <div className="mt-4 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+                            <div>
+                                <div className="my-5">
+                                    <h2 className="font-semibold text-xl">Description</h2>
+                                    {accomodation.description}
+                                </div>
+                                Check-In: {accomodation.checkIn} <br />
+                                Check-Out: {accomodation.checkOut} <br />
+                                Max Guests: {accomodation.maxGuests} <br />
+                            </div>
+                            <div>
+                                <Booking accomodation={accomodation}/>
+                            </div>
                     </div>
             </div>
         </div>
